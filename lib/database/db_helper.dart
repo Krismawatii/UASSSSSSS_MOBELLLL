527d91105bd8a0f065a1dc221e22b176ad6c448a
Krismawati <krismawatti11@gmail.com>
2022-12-18 23:34:01 +0700

    update backend

---
+++ lib/database/db_helper.dart
@@ -0,0 +1,82 @@
+import 'package:uas_kelompok3/models/item.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite/sqlite_api.dart';
+import 'package:path/path.dart';
+
+class DbHelper {
+  static final DbHelper _instance = DbHelper._internal();
+  static Database? _database;
+
+  final String tableName = 'tableItem';
+  final String columnId = 'id';
+  final String columnName = 'nama';
+  final String columnAlamat = 'alamat';
+  final String columnNim = 'nim';
+
+  DbHelper._internal();
+   factory DbHelper() => _instance;
+
+
+
+    Future<Database?> get _db  async {
+        if (_database != null) {
+            return _database;
+        }
+        _database = await _initDb();
+        return _database;
+    }
+
+
+    Future<Database?> _initDb() async {
+        String databasePath = await getDatabasesPath();
+        String path = join(databasePath, 'item.db');
+        
+        return await openDatabase(path, version: 1, onCreate: _onCreate);
+    }
+
+    //membuat tabel dan field-fieldnya
+    Future<void> _onCreate(Database db, int version) async {
+        var sql = "CREATE TABLE $tableName($columnId INTEGER PRIMARY KEY, "
+            "$columnName TEXT,"
+            "$columnAlamat TEXT,"
+            "$columnNim TEXT,"
+            
+             await db.execute(sql);
+    }
+
+    //insert
+     Future<int?> saveItem(Item item) async {
+        var dbClient = await _db;
+        return await dbClient!.insert(tableName, item.toMap());
+    }
+
+    //read database
+    Future<List?> getAllItem() async {
+        var dbClient = await _db;
+        var result = await dbClient!.query(tableName, columns: [
+            columnId,
+            columnName,
+            columnAlamat,
+            columnNim,
+        ]);
+        
+        return result.toList();
+    }
+
+    //update database
+    Future<int?> updateItem(Item item) async {
+        var dbClient = await _db;
+        return await dbClient!.update(tableName, item.toMap(), where: '$columnId = ?', whereArgs: [item.id]);
+
+    }
+
+     //hapus database
+    Future<int?> deleteItem(int id) async {
+        var dbClient = await _db;
+        return await dbClient!.delete(tableName, where: '$columnId = ?', whereArgs: [id]);
+    }
+
+
+
+}
+
--- lib/models/item.dart
+++ lib/models/item.dart
@@ -1,33 +1,27 @@
-import 'dart:html';
-
 class Item {
-  String _nama;
-  String _alamat;
-  int _nim;
+  int? id;
+  String? nama;
+  String? alamat;
+  int? nim;
 
-  String get nama => this._nama;
-  set nama(String value) => this._nama = value;
-
-  String get alamat => this._alamat;
-  set alamat(String value) => this._alamat = value;
-
-  get nim => this._nim;
-  set nim(value) => this._nim = value;
-
-  Item(this._nama, this._alamat, this._nim);
-
-//Konstruktor konversi dari Map ke Item
-  Item.fromMap(Map<String, dynamic> map) {
-    this._nama = map['nama'];
-    this._alamat = map['alamat'];
-    this._nim = map['nim'];
-  }
+  Item({this.id, this.nama, this.alamat, this.nim});
 
   Map<String, dynamic> toMap() {
-    Map<String, dynamic> map = Map<String, dynamic>();
-    map['nama'] = this._nama;
+    var map = Map<String, dynamic>();
+    if (id != null) {
+      map['id'] = id;
+    }
+    map['nama'] = nama;
     map['alamat'] = alamat;
     map['nim'] = nim;
+
     return map;
+  }
+
+  Item.fromMap(Map<String, dynamic> map) {
+    this.id = map['id'];
+    this.nama = map['nama'];
+    this.alamat = map['alamat'];
+    this.nim = map['nim'];
   }
 }
