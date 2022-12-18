32b8f9e762351c51300d94318e1d03e32a7b47cb
K2FA <khalidarrafi6331@gmail.com>
2022-12-18 22:57:19 +0700

    homepage view

--- lib/models/item.dart
+++ lib/models/item.dart
@@ -1,33 +1,33 @@
-import 'dart:html';
+// import 'dart:html';
 
-class Item {
-  String _nama;
-  String _alamat;
-  int _nim;
+// class Item {
+//   String _nama;
+//   String _alamat;
+//   int _nim;
 
-  String get nama => this._nama;
-  set nama(String value) => this._nama = value;
+//   String get nama => this._nama;
+//   set nama(String value) => this._nama = value;
 
-  String get alamat => this._alamat;
-  set alamat(String value) => this._alamat = value;
+//   String get alamat => this._alamat;
+//   set alamat(String value) => this._alamat = value;
 
-  get nim => this._nim;
-  set nim(value) => this._nim = value;
+//   get nim => this._nim;
+//   set nim(value) => this._nim = value;
 
-  Item(this._nama, this._alamat, this._nim);
+//   Item(this._nama, this._alamat, this._nim);
 
-//Konstruktor konversi dari Map ke Item
-  Item.fromMap(Map<String, dynamic> map) {
-    this._nama = map['nama'];
-    this._alamat = map['alamat'];
-    this._nim = map['nim'];
-  }
+// //Konstruktor konversi dari Map ke Item
+//   Item.fromMap(Map<String, dynamic> map) {
+//     this._nama = map['nama'];
+//     this._alamat = map['alamat'];
+//     this._nim = map['nim'];
+//   }
 
-  Map<String, dynamic> toMap() {
-    Map<String, dynamic> map = Map<String, dynamic>();
-    map['nama'] = this._nama;
-    map['alamat'] = alamat;
-    map['nim'] = nim;
-    return map;
-  }
-}
+//   Map<String, dynamic> toMap() {
+//     Map<String, dynamic> map = Map<String, dynamic>();
+//     map['nama'] = this._nama;
+//     map['alamat'] = alamat;
+//     map['nim'] = nim;
+//     return map;
+//   }
+// }
