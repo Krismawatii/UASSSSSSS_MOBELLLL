ea265f2047c7f1e96c40d01fc12d6cc6a0ae3127
K2FA <khalidarrafi6331@gmail.com>
2022-12-20 09:39:07 +0700

    modified home_page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,7 +15,7 @@ class _Homepage extends State<HomePage> {
   Gender? _gender = Gender.male;
 
   // Model
-  Item item;
+  // Item item;
 
   // Controller
   final nimController = TextEditingController();
@@ -28,101 +28,127 @@ class _Homepage extends State<HomePage> {
         home: DefaultTabController(
       length: 2,
       child: Scaffold(
-        appBar: AppBar(
-          title: Text("UAS_KELOMPOK3"),
-          bottom: const TabBar(
-            tabs: [
-              Tab(text: "Biodata"),
-              Tab(text: "List Biodata"),
+          appBar: AppBar(
+            title: Text("UAS_KELOMPOK3"),
+            bottom: const TabBar(
+              tabs: <Widget>[
+                Tab(text: "Biodata"),
+                Tab(text: "List Biodata"),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              Column(
+                children: [
+                  // ~~~~~~~~~~~~~~~~ Tittle ~~~~~~~~~~~~~~~~
+                  Container(
+                    child: Text(
+                      'BIODATA',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1.5,
+                      ),
+                    ),
+                    margin: const EdgeInsets.only(top: 50),
+                  ),
+                  // ~~~~~~~~~~~~~~ End of Input Tittle ~~~~~~~~~~~~~~~~~~~
+
+                  // ~~~~~~~~~~~~~~~~ Input NIM ~~~~~~~~~~~~~~~~
+                  Container(
+                    child: TextField(
+                      decoration: InputDecoration(label: Text('NIM :')),
+                      keyboardType: TextInputType.number,
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      // controller: nim,
+                    ),
+                    margin: const EdgeInsets.only(left: 50, right: 50, top: 20),
+                  ),
+                  // ~~~~~~~~~~~~~~ End of Input NIM ~~~~~~~~~~~~~~~~~~~
+
+                  // ~~~~~~~~~~~~~~~~ Input Nama ~~~~~~~~~~~~~~~~
+                  Container(
+                    child: TextField(
+                      decoration: InputDecoration(label: Text('Name :')),
+                      // controller: nama,
+                    ),
+                    margin: const EdgeInsets.only(left: 50, right: 50, top: 10),
+                    // margin: const EdgeInsets.only(top: 20),,
+                  ),
+                  // ~~~~~~~~~~~~~~ End of Input Nama ~~~~~~~~~~~~~~~~~~~
+
+                  // ~~~~~~~~~~~~~~~~ Input Alamat ~~~~~~~~~~~~~~~~
+                  Container(
+                    child: TextField(
+                      decoration: InputDecoration(label: Text('Alamat :')),
+                      // controller: alamat,
+                    ),
+                    margin: const EdgeInsets.only(left: 50, right: 50, top: 10),
+                    // margin: const EdgeInsets.only(top: 20),,
+                  ),
+                  // ~~~~~~~~~~~~~~ End of Input Nama ~~~~~~~~~~~~~~~~~~~
+
+                  // ~~~~~~~~~~~~~~~~ Radio Button ~~~~~~~~~~~~~~~~
+                  Container(
+                    child: Row(children: <Widget>[
+                      Expanded(
+                        child: ListTile(
+                          title: Text('Laki-Laki'),
+                          leading: Radio<Gender>(
+                            value: Gender.male,
+                            groupValue: _gender,
+                            onChanged: ((Gender? value) {
+                              setState(() {
+                                _gender = value;
+                              });
+                            }),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: ListTile(
+                          title: Text('Perempuan'),
+                          leading: Radio<Gender>(
+                            value: Gender.female,
+                            groupValue: _gender,
+                            onChanged: ((Gender? value) {
+                              setState(() {
+                                _gender = value;
+                              });
+                            }),
+                          ),
+                        ),
+                      ),
+                    ]),
+                    margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
+                  ),
+                  Container(
+                      margin: EdgeInsets.only(top: 20),
+                      child: ElevatedButton(
+                        child: Text("Submit"),
+                        onPressed: () {
+                          // item = Item(
+                          //   nimController.toString(),
+                          //   namaController.text,
+                          //   alamatController.text,
+                          // );
+                        },
+                        style: ElevatedButton.styleFrom(
+                            padding: EdgeInsets.only(left: 30, right: 30)),
+                      )),
+                  // ~~~~~~~~~~~~~~ End of Radio Button ~~~~~~~~~~~~~~~~~~~
+                ],
+              ),
+              Column(
+                children: [
+                  Container(
+                    child: Text('Test'),
+                  )
+                ],
+              )
             ],
-          ),
-        ),
-        body: Column(children: [
-          Container(
-              child: Text(
-                'BIODATA',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 1.5,
-                ),
-              ),
-              margin: const EdgeInsets.only(top: 50)),
-          Container(
-            child: TextField(
-              decoration: InputDecoration(label: Text('NIM :')),
-              keyboardType: TextInputType.number,
-              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-              controller: nim,
-            ),
-            margin: const EdgeInsets.only(left: 50, right: 50, top: 20),
-            // margin: const EdgeInsets.only(top: 20),
-          ),
-          Container(
-            child: TextField(
-              decoration: InputDecoration(label: Text('Name :')),
-              controller: nama,
-            ),
-            margin: const EdgeInsets.only(left: 50, right: 50, top: 10),
-            // margin: const EdgeInsets.only(top: 20),,
-          ),
-          Container(
-            child: TextField(
-              decoration: InputDecoration(label: Text('Alamat :')),
-              controller: alamat,
-            ),
-            margin: const EdgeInsets.only(left: 50, right: 50, top: 10),
-            // margin: const EdgeInsets.only(top: 20),,
-          ),
-          Container(
-            child: Row(children: <Widget>[
-              Expanded(
-                child: ListTile(
-                  title: Text('Laki-Laki'),
-                  leading: Radio<Gender>(
-                    value: Gender.male,
-                    groupValue: _gender,
-                    onChanged: ((Gender? value) {
-                      setState(() {
-                        _gender = value;
-                      });
-                    }),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: ListTile(
-                  title: Text('Perempuan'),
-                  leading: Radio<Gender>(
-                    value: Gender.female,
-                    groupValue: _gender,
-                    onChanged: ((Gender? value) {
-                      setState(() {
-                        _gender = value;
-                      });
-                    }),
-                  ),
-                ),
-              ),
-            ]),
-            margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
-          ),
-          Container(
-              margin: EdgeInsets.only(top: 20),
-              child: ElevatedButton(
-                child: Text("Submit"),
-                onPressed: () {
-                  item = Item(
-                    nimController.toString(),
-                    namaController.text,
-                    alamatController.text,
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                    padding: EdgeInsets.only(left: 30, right: 30)),
-              )),
-        ]),
-      ),
+          )),
     ));
   }
 }
