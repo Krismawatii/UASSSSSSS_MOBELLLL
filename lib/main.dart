6e14515d4fd9fc2f68387b1bdbcafaa913412f1f
Krismawati <krismawatti11@gmail.com>
2022-12-26 19:38:31 +0700

    home

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
 // import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:uas_kelompok3/pages/home_page.dart';
-import 'package:uas_kelompok3/pages/item_page.dart';
+import 'package:uas_kelompok3/pages/page_biodata.dart';
+import 'package:uas_kelompok3/pages/list_data.dart';
 
 void main() {
   runApp(MaterialApp(
     initialRoute: '/',
     routes: {
-      '/': (context) => HomePage(),
-      '/item': (context) => ItemPage(),
+      '/': (context) => BiodataPage(),
     },
   ));
 }
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:uas_kelompok3/models/item.dart';
+import 'package:uas_kelompok3/pages/page_biodata.dart';
+
+class HomePage extends StatelessWidget {
+  List<Item> itemList;
+
+  @override
+  Widget build(BuildContext) {
+    if (itemList == null) {
+      itemList = List<Item>();
+    }
+
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+        title: Text("KELOMPOK 3"),
+      )),
+    );
+  }
+}
