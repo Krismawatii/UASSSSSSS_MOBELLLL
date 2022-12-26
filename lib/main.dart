a39ffdf6df4616176917dfb133d8ffc09336a4ab
K2FA <khalidarrafi6331@gmail.com>
2022-12-26 23:25:00 +0700

    navigate home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 // import 'package:flutter/cupertino.dart';
+// import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:uas_kelompok3/pages/biodata_page.dart';
+import 'package:uas_kelompok3/pages/home_page.dart';
 import 'package:uas_kelompok3/pages/list_data.dart';
 import 'package:uas_kelompok3/pages/tab_page.dart';
 
@@ -8,7 +11,7 @@ void main() {
   runApp(MaterialApp(
     initialRoute: '/',
     routes: {
-      '/': (context) => TabPage(),
+      '/': (context) => HomePage(),
     },
   ));
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,21 +2,39 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:uas_kelompok3/models/item.dart';
 import 'package:uas_kelompok3/pages/biodata_page.dart';
+import 'package:uas_kelompok3/pages/tab_page.dart';
 
 class HomePage extends StatelessWidget {
   List<Item> itemList;
 
   @override
-  Widget build(BuildContext) {
+  Widget build(BuildContext context) {
     if (itemList == null) {
       itemList = List<Item>();
     }
 
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-        title: Text("KELOMPOK 3"),
-      )),
+        appBar: AppBar(
+          title: Text("KELOMPOK 3"),
+        ),
+        body: Center(
+            child: SizedBox(
+          width: 200.0,
+          height: 100.0,
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => TabPage()),
+              );
+            },
+            style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.only(left: 30, right: 30)),
+            child: Text('Start'),
+          ),
+        )),
+      ),
     );
 
     //WEI BUTTON START TEK OPO NGUNU YA, CEK APIK
